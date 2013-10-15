12f468d95530d9ef9fc0b118ca627d91a3abf487
Blaise Ritchie <blaise@jizzyjugs.com>
2013-10-15 14:55:36 -0700

    Added support for multiple subreddits

--- reddit.py
+++ reddit.py
@@ -9,45 +9,56 @@ exts = ['.png', '.jpg', '.jpeg']
 
 def setReddit():
 	hdr = {'User-Agent' : "Mozilla/5.0 (X11; Linux x86_64; rv:24.0) Gecko/20100101 Firefox/24.0"}
-	baseurl = "http://reddit.com/r/" + Settings.SUBREDDIT + "/" + Settings.PAGE + ".json"
-
-	after = None
 
 	set = False
 
-	while not set:
-		url = baseurl
-		if after != None:
-			url += "?after=" + after
-		req = urllib.request.Request(url, headers=hdr)
-		page = json.loads(urllib.request.urlopen(req).read().decode('utf8'))
+	subreddits = []
 
-		if page['kind'] == "Listing":
-			posts = copy.copy(page['data']['children'])
-			random.shuffle(posts)
-			for post in posts:
-				dims = re.search(r'(?P<width>[0-9]+) ?x ?(?P<height>[0-9]+)', post['data']['title'])
-				if dims == None:
-					continue
-				width = int(dims.group('width'))
-				height = int(dims.group('height'))
-				ext = os.path.splitext(post['data']['url'])[1]
-				if ext.lower() in exts:
-					fname = post['data']['id'] + ext
-					path = Settings.DIRECTORY + "/" + fname 
-					f = open(path, "wb+")
-					f.write(urllib.request.urlopen(post['data']['url']).read())
-					set = fromstr(Settings.WALLMANAGER)(path, width, height)
+	if hasattr(Settings, "SUBREDDITS"):
+		subreddits += Settings.SUBREDDITS
 
-					if set:
-						print("Set: " + fname + ": " + str(width) + "x" + str(height))
+	if hasattr(Settings, "SUBREDDIT") and not Settings.SUBREDDIT in subreddits:
+		subreddits.append(Settings.SUBREDDIT)
 
+	random.shuffle(subreddits)
+
+	for subreddit in subreddits:
+		baseurl = "http://reddit.com/r/" + subreddit + "/" + Settings.PAGE + ".json"
+
+		after = None
+		page = 0
+
+		while not set:
+			url = baseurl
+			if after != None:
+				url += "?after=" + after
+			req = urllib.request.Request(url, headers=hdr)
+			page = json.loads(urllib.request.urlopen(req).read().decode('utf8'))
+
+			if page['kind'] == "Listing":
+				posts = copy.copy(page['data']['children'])
+				random.shuffle(posts)
+				for post in posts:
+					dims = re.search(r'(?P<width>[0-9]+) ?x ?(?P<height>[0-9]+)', post['data']['title'])
+					if dims == None:
+						continue
+					width = int(dims.group('width'))
+					height = int(dims.group('height'))
+					ext = os.path.splitext(post['data']['url'])[1]
+					if ext.lower() in exts:
+						fname = post['data']['id'] + ext
+						path = Settings.DIRECTORY + "/" + fname 
+						f = open(path, "wb+")
+						f.write(urllib.request.urlopen(post['data']['url']).read())
+						set = fromstr(Settings.WALLMANAGER)(path, width, height)
+
+						if set:
+							print("Set: " + fname + ": " + str(width) + "x" + str(height))
+
+						
+				if not set:
+					if len(posts) <= 0 or page > Settings.MAXPAGES:
 						break
-			if not set:
-				if len(posts) <= 0:
-					return False
-				after = page['data']['children'][-1]['data']['name']
-		else:
-			return False
-
-	return True
+					page += 1
+					after = page['data']['children'][-1]['data']['name']
+	return set
